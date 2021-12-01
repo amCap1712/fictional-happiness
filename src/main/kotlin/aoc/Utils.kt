34c0260ac73fe6760324b0b23537fba985aadf05
Kartik Ohri <kartikohri13@gmail.com>
2021-12-01 15:30:48 +0530

    Rearrange files and add day 1 solution

---
+++ src/main/kotlin/aoc/Utils.kt
@@ -0,0 +1,21 @@
+package aoc
+
+import java.io.File
+import java.math.BigInteger
+import java.security.MessageDigest
+
+
+object Utils {
+    /**
+     * Reads lines from the given input txt file.
+     */
+    fun readInput(number: String) = File(Utils.javaClass.getResource("Day$number.txt")?.path!!).readLines()
+
+    fun readInputAsInts(day: Int) = readInput(day.toString()).map { it.toInt() }
+
+    /**
+     * Converts string to aoc.md5 hash.
+     */
+    fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+}
